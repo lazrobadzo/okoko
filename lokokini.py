5b07afdaf07d28867482fae4e89770a23102e170
lazrobadzo <144172921+lazrobadzo@users.noreply.github.com>
2023-10-04 10:35:46 -0700

    Add files via upload

---
+++ lokokini.py
@@ -0,0 +1,10 @@
+username =input("what is your name")
+lastname =input("what is your lastname")
+dedname =input("what is your ded name")
+momname =input("what is your mom name")
+petsname =input("what is your pets name")
+myage =input("what is your age")
+
+print("username + lastname + dedname + momname + petsname + myage")
+
+print(myage+15)
